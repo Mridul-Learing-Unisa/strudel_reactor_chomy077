81ef5eaaf39f66a21ce46fcf777fa52aec429850
Mridul-Learing-Unisa <chomy077@mymail.unisa.edu.au>
2025-10-27 23:48:37 +1030

    UI and changing js to react

--- src/components/DJ_controlls.jsx
+++ src/components/DJ_controlls.jsx
@@ -1,18 +1,46 @@
+﻿
 function DJcontrolls() {
+    const BG = "https://cdn.mos.cms.futurecdn.net/2HFwv3bcoyZwtdG2tVBLv7-650-80.jpg.webp"
     return (
         <>
-            <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                    p1: ON
-                 </label>
+            <div className="card shadow border-0" style={{ backgroundImage: `url(${BG})`, backgroundSize: "cover", backgroundPosition: "center", borderRadius: "12px" }}>
+            
+                <div className="card shadow border-0" style={{  background: 'black', opacity: '0.7', color: "white" }}>
+                <div className="card-body">
+                    <h5 className="card-title mb-3 text-warning" style={{ textAlign: "center" }} >DJ Controls</h5>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="cmp_label">Set CPM</span>
+                        <input type="text" className="form-control" id = "cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
+                    </div>
+
+                    
+                    <label htmlFor="volume_range" className="form-label">Volume</label>
+                        <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
+
+
+                    <label htmlFor="tracks" className="form-label">Tracks</label>
+
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" style={{ accentColor: 'gold' }} id="s1"/>
+                        <label className="form-check-label" htmlFor="s1">
+                            s1
+                         </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="d1" />
+                        <label className="form-check-label" htmlFor="d1">
+                            d1
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="d2" />
+                        <label className="form-check-label" htmlFor="d1">
+                            d2
+                        </label>
+                    </div>
+                </div>
              </div>
-             <div className="form-check">
-                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                 <label className="form-check-label" htmlFor="flexRadioDefault2">
-                     p1: HUSH
-                 </label>
-            </div>
+           </div>
       </>
   );
 }

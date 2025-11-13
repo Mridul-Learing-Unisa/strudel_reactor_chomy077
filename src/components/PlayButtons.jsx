70e53f07f33378f7c313be2ea1c498e6214be347
Mridul-Learing-Unisa <chomy077@mymail.unisa.edu.au>
2025-11-14 05:47:11 +1030

    Updated UI for play and pause buttons

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -1,10 +1,11 @@
-﻿
+﻿import { FaPlay, FaStop } from "react-icons/fa";
+
 function PlayButtons({ onPlay, onStop }) {
   return (
       <>
           <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-              <button id="play" className="btn btn-outline-success" onClick={onPlay}>Play</button>
-              <button id="stop" className="btn btn-outline-danger" onClick={onStop}>Stop</button>
+              <button id="play" className="btn btn-outline-success" onClick={onPlay}><FaPlay /></button>
+              <button id="stop" className="btn btn-outline-danger" onClick={onStop}><FaStop /></button>
           </div>
       </>
   );
